78fa220cabf2bf1cc7db0f5353c5b0b0bb5f8623
EwanMostert <ewanmostert07@gmail.com>
2024-05-01 16:11:48 +0200

    May Day Update

--- Python/motor_drive_test.py
+++ Python/motor_drive_test.py
@@ -1,14 +1,38 @@
 import RPi.GPIO as GPIO
 from time import sleep
 
-motor1 = 12
+motor1_rev= 12
+motor1_fwd = 13
 GPIO.setwarnings(False)
 GPIO.setmode(GPIO.BOARD)
-GPIO.setup(motor1,GPIO.OUT)
-motor1_pwm = GPIO.PWM(motor1,1000)
-motor1_pwm.start(0)
 
-D = 0.1
+GPIO.setup(motor1_fwd,GPIO.OUT)
+motor1_fwd_pwm = GPIO.PWM(motor1_fwd,10000)
+motor1_fwd_pwm.start(0)
 
-while True:
-    motor1_pwm.ChangeDutyCycle(D)
+GPIO.setup(motor1_rev,GPIO.OUT)
+motor1_rev_pwm = GPIO.PWM(motor1_rev,10000)
+motor1_rev_pwm.start(0)
+
+for duty in range(0,101,1):
+    motor1_fwd_pwm.ChangeDutyCycle(duty)
+    motor1_rev_pwm.ChangeDutyCycle(0)
+    sleep(0.01)
+sleep(1)
+
+for duty in range(100,-1,-1):
+    motor1_fwd_pwm.ChangeDutyCycle(duty)
+    motor1_rev_pwm.ChangeDutyCycle(0)
+    sleep(0.01)
+sleep(2)
+
+for duty in range(0,101,1):
+    motor1_fwd_pwm.ChangeDutyCycle(0)
+    motor1_rev_pwm.ChangeDutyCycle(duty)
+    sleep(0.01)
+sleep(1)
+
+for duty in range(100,-1,-1):
+    motor1_fwd_pwm.ChangeDutyCycle(0)
+    motor1_rev_pwm.ChangeDutyCycle(duty)
+    sleep(0.01)
