19e941d89e7855f7470276c3d786984cacc224e0
EwanMostert <ewanmostert07@gmail.com>
2024-05-07 22:31:10 +0200

    yay dynamic image sending

--- Python/full_system.py
+++ Python/full_system.py
@@ -7,55 +7,60 @@ import time
 import serial
 import string
 import pynmea2
-from math import *
+import threading
 
+from gpiozero import PWMOutputDevice
+from math import *
 from gps import *
 from pathlib import Path
 from picamera2 import *
 #------------------------------------------------
 
 
-#System setup:
-running = True
-start_time = time.time()
-#------------------------------------------------
-
-
 #network setup
 port = "34:F3:9A:CA:76:E0"
-bt_timeout = 0
+bt_connected = False
+server_sock = socket.socket(socket.AF_BLUETOOTH, socket.SOCK_STREAM, socket.BTPROTO_RFCOMM)
+
+def try_bt_connect(bt_running):
+    global server_sock
+    global bt_connected
+    while bt_running.is_set():
+        if bt_connected == False:            
+            try:
+                server_sock.connect((port,10))
+                print("Connected to server")
+                bt_connected = True
+            except:
+                print("Not connected to server")
+                server_sock = socket.socket(socket.AF_BLUETOOTH, socket.SOCK_STREAM, socket.BTPROTO_RFCOMM)               
+        time.sleep(10.0)   
+    return 
 #------------------------------------------------
 
 
 #motor control setup
-motor1_fwd = 22
-motor1_rev = 24
-motor2_fwd = 26
-motor2_rev = 28
+motor1_fwd = PWMOutputDevice(25)
+motor1_rev = PWMOutputDevice(8)
+motor2_fwd = PWMOutputDevice(7)
+motor2_rev = PWMOutputDevice(1)
 
 D = 0
 d_1 = 0
 d_2 = 0
 
-GPIO.setwarnings(False)
-GPIO.setmode(GPIO.BOARD)
+# GPIO.setwarnings(False)
+# GPIO.setmode(GPIO.BOARD)
 
-GPIO.setup(motor1_fwd,GPIO.OUT)
-motor1_fwd_pwm = GPIO.PWM(motor1_fwd,10000)
-GPIO.setup(motor1_rev,GPIO.OUT)
-motor1_rev_pwm = GPIO.PWM(motor1_rev,10000)
+# GPIO.setup(motor1_fwd,GPIO.OUT)
+# motor1_fwd_pwm = GPIO.PWM(motor1_fwd,10000)
+# GPIO.setup(motor1_rev,GPIO.OUT)
+# motor1_rev_pwm = GPIO.PWM(motor1_rev,10000)
 
-GPIO.setup(motor2_fwd,GPIO.OUT)
-motor2_fwd_pwm = GPIO.PWM(motor2_fwd,10000)
-GPIO.setup(motor2_rev,GPIO.OUT)
-motor2_rev_pwm = GPIO.PWM(motor2_rev,10000)
-#------------------------------------------------
-
-
-#camera setup
-# picam2 = Picamera2()
-# camera_config = picam2.create_preview_configuration()
-# picam2.configure(camera_config)
+# GPIO.setup(motor2_fwd,GPIO.OUT)
+# motor2_fwd_pwm = GPIO.PWM(motor2_fwd,10000)
+# GPIO.setup(motor2_rev,GPIO.OUT)
+# motor2_rev_pwm = GPIO.PWM(motor2_rev,10000)
 #------------------------------------------------
 
 
@@ -64,6 +69,13 @@ last_image_taken = 0
 last_image_sent = 0
 img_name = ""
 image = ""
+#------------------------------------------------
+
+
+#camera setup
+picam2 = Picamera2()
+camera_config = picam2.create_preview_configuration()
+picam2.configure(camera_config)
 #------------------------------------------------
 
 
@@ -110,86 +122,80 @@ except:
 
 
 #GPS test
-count = 0
-time_1 = 0
-time_2 = 0
-on_time = False
-for i in range(100):        
-    dataout = pynmea2.NMEAStreamReader()
-    try:                        
-        newdata = ser.readline().decode()
-        if newdata[0:10].__contains__("$GPRMC"):
-            if on_time == False:
-                time_1 = time.time()
-                on_time = True
-            else:
-                time_2 = time.time()
-                on_time = False    
-            newmsg = pynmea2.parse(newdata)
-            lat = newmsg.latitude
-            lng = newmsg.longitude
-            gps = "Latitude=" + str(lat) + " and Longitude=" + str(lng)
-            print(gps)
-            count += 1
-    except:
-        print("Could not read from serial channel")
-    print(time_2 - time_1)
+# count = 0
+# time_1 = 0
+# time_2 = 0
+# on_time = False
+# for i in range(100):        
+#     dataout = pynmea2.NMEAStreamReader()
+#     try:                        
+#         newdata = ser.readline().decode()
+#         if newdata[0:10].__contains__("$GPRMC"):
+#             if on_time == False:
+#                 time_1 = time.time()
+#                 on_time = True
+#             else:
+#                 time_2 = time.time()
+#                 on_time = False    
+#             newmsg = pynmea2.parse(newdata)
+#             lat = newmsg.latitude
+#             lng = newmsg.longitude
+#             gps = "Latitude=" + str(lat) + " and Longitude=" + str(lng)
+#             print(gps)
+#             count += 1
+#     except:
+#         print("Could not read from serial channel")
+#     print(time_2 - time_1)
 #------------------------------------------------
 
 
 #function for taking photo
 def take_photo():
+    global last_image_taken
+    global img_name
+    global picam2
     img_name = "image" + str(last_image_taken) + ".jpg"
-    picam2.start_preview(Preview.QTGL)
     picam2.start()
-    time.sleep(2)
     picam2.capture_file(img_name)
-    last_image_taken += 1    
+    last_image_taken += 1
+    return   
 #------------------------------------------------
 
 
 #function for sending photo
 def send_photo():
+    global last_image_sent
+    global img_name
+    global server_sock
+    global bt_connected
     try:
-    # open image
+    #open image        
         img_name = "image" + str(last_image_sent) + ".jpg"
-        image = os.getcwd() + img_name
+        image = os.getcwd() + "/" + img_name
         myfile = open(image, 'r+b')
         bytes = myfile.read()
         size = len(bytes)
 
-    #send image
-        while bytes:
-            server_sock.sendall(bytes)
-            bytes = myfile.read()
-        myfile.close()
-        last_image_sent += 1
-    
-    # # send image size to server
-    # server_sock.sendall(("SIZE %s" % size).encode())
-    # answer = server_sock.recv(4096)
-
-    # print(f"answer = {answer}")
-
-    # # send image to server
-    # txt = str(answer)
-    # if txt.__contains__('GOT SIZE'):        
-    #     server_sock.sendall(bytes)
-    #     print("hello")
-
-    #     # check what server send
-    #     answer = server_sock.recv(4096)
-    #     txt = str(answer)
-    #     print('answer = %s' % answer)
-
-    #     if txt.__contains__('GOT IMAGE'):
-    #         server_sock.sendall("BYE BYE ".encode())
-    #         print('Image successfully send to server')
-
-    # myfile.close()
+    #request to send data
+        message = (b'sending data')
+        print("Raising hand")
+        server_sock.send(message)
+        answer = server_sock.recv(1024).decode()
+        if answer.__contains__("go ahead"):
+            print("Begin data send")
+            #send image
+            while bytes:
+                server_sock.sendall(bytes)
+                bytes = myfile.read()
+            myfile.close()
+            last_image_sent += 1
+            bt_connected = False
 
     finally:
         server_sock.close()
+        print("Disconnected from server")
+        bt_connected = False
+    return
 
 #BLUETOOTH PROTOCOL:
 #After initial connection of the socket, the communication protocol between server (PC) and client (RPi) will be as follows:
@@ -314,17 +320,30 @@ def activate_motors():
 #------------------------------------------------
 
 
+#System setup:
+running = True
+# print("Let's begin")
+start_time = time.time()
+#------------------------------------------------
+
+
 #Main system loop:
-    while running == True:
-        if bt_timeout >= 
-        try:
-            server_sock=socket.socket(socket.AF_BLUETOOTH, socket.SOCK_STREAM, socket.BTPROTO_RFCOMM)
-            server_sock.connect((port,10))
-        except:
-            print("Could not connect bluetooth")
-        get_pos()
-        if calc_distance(pos_x_new,pos_y_new,pos_x_old,pos_y_old) >= 2.5:
-            pos_change = calc_distance(pos_x_new,pos_y_new,pos_x_old,pos_y_old)
-            calc_speed()
-            angle = calc_angle(pos_x_new,pos_y_new,pos_x_old,pos_y_old)
+bt_running = threading.Event()
+bt_running.set()
+bt_thread = threading.Thread(target=try_bt_connect, args=(bt_running,))
+bt_thread.start()
+
+while (running == True):
+    # get_pos()
+    # if calc_distance(pos_x_new,pos_y_new,pos_x_old,pos_y_old) >= 2.5:
+    #     pos_change = calc_distance(pos_x_new,pos_y_new,pos_x_old,pos_y_old)
+    #     calc_speed()
+    #     angle = calc_angle(pos_x_new,pos_y_new,pos_x_old,pos_y_old)
+    if bt_connected == True:
+        take_photo()
+        send_photo()
+    time.sleep(10)
+
+bt_running.clear()  
+    
 #------------------------------------------------
