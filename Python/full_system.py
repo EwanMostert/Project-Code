208d5cf75e4bdb7ac6306a3df8651c8b3bb30f93
EwanMostert <ewanmostert07@gmail.com>
2024-05-07 17:49:44 +0200

    May 7th

--- Python/full_system.py
+++ Python/full_system.py
@@ -22,35 +22,33 @@ start_time = time.time()
 
 
 #network setup
-# port = "34:F3:9A:CA:76:E0"
-
-# server_sock=socket.socket(socket.AF_BLUETOOTH, socket.SOCK_STREAM, socket.BTPROTO_RFCOMM)
-# server_sock.connect((port,10))
+port = "34:F3:9A:CA:76:E0"
+bt_timeout = 0
 #------------------------------------------------
 
 
 #motor control setup
-# motor1_fwd = 22
-# motor1_rev = 24
-# motor2_fwd = 26
-# motor2_rev = 28
+motor1_fwd = 22
+motor1_rev = 24
+motor2_fwd = 26
+motor2_rev = 28
 
-#D = 0
-#d_1 = 0
-#d_2 = 0
+D = 0
+d_1 = 0
+d_2 = 0
 
-# GPIO.setwarnings(False)
-# GPIO.setmode(GPIO.BOARD)
+GPIO.setwarnings(False)
+GPIO.setmode(GPIO.BOARD)
 
-# GPIO.setup(motor1_fwd,GPIO.OUT)
-# motor1_fwd_pwm = GPIO.PWM(motor1_fwd,10000)
-# GPIO.setup(motor1_rev,GPIO.OUT)
-# motor1_rev_pwm = GPIO.PWM(motor1_rev,10000)
+GPIO.setup(motor1_fwd,GPIO.OUT)
+motor1_fwd_pwm = GPIO.PWM(motor1_fwd,10000)
+GPIO.setup(motor1_rev,GPIO.OUT)
+motor1_rev_pwm = GPIO.PWM(motor1_rev,10000)
 
-# GPIO.setup(motor2_fwd,GPIO.OUT)
-# motor2_fwd_pwm = GPIO.PWM(motor2_fwd,10000)
-# GPIO.setup(motor2_rev,GPIO.OUT)
-# motor2_rev_pwm = GPIO.PWM(motor2_rev,10000)
+GPIO.setup(motor2_fwd,GPIO.OUT)
+motor2_fwd_pwm = GPIO.PWM(motor2_fwd,10000)
+GPIO.setup(motor2_rev,GPIO.OUT)
+motor2_rev_pwm = GPIO.PWM(motor2_rev,10000)
 #------------------------------------------------
 
 
@@ -70,6 +68,8 @@ image = ""
 
 
 #positional data init
+ROTATION_PERIOD = 10
+
 pos_x_new = 0
 pos_y_new = 0
 pos_x_old = 0
@@ -86,8 +86,8 @@ goal_angle = 0
 speed = 0
 angle = 90
 
+dist_err = 0
 angle_err = 0
-distance_err = 0
 
 time_new = 0
 time_old = 0
@@ -228,43 +228,103 @@ def get_pos():
 
 
 #Calculate heading and speed and distance:
-def get_distance():
-    angle_x = pos_x_new - pos_x_old
-    angle_y = pos_y_new - pos_y_old
+def calc_distance(pos_x_2,pos_y_2,pos_x_1,pos_y_1):
+    angle_x = pos_x_2 - pos_x_1
+    angle_y = pos_y_2 - pos_y_1
     angle_change = radians(sqrt(pow(angle_x,2) + pow(angle_y,2)))
     result = angle_change * 6371 * 1000
     return result
 
-def get_speed():
+def calc_speed():
     speed = pos_change / (time_new - time_old)
 
-def get_angle():
-    angle = degrees(atan((pos_x_new - pos_x_old)/(pos_y_new - pos_y_old)))
+def calc_angle(pos_x_2,pos_y_2,pos_x_1,pos_y_1):
+    result = degrees(atan((pos_x_2 - pos_x_1)/(pos_y_2 - pos_y_1)))
+    return result
 
-def get_error():
-    return
+def calc_error():
+    dist_err = calc_distance(goal_x,goal_y,pos_x_new,pos_y_new)
+    angle_err = angle - calc_angle(goal_x,goal_y,pos_x_new,pos_y_new)
 
 #------------------------------------------------
 
 
 #Actuation functions:
 def set_motorspeed():
-    return
+    if dist_err > 5:
+        D = 100
+    else:
+        D = 50
+    d_1 *= D
+    d_2 *= D
+
+def ramp_up():
+    for i in range(100,-1,-1):
+        D = i
+        d_1 = D
+        d_2 = D
+        activate_motors()
+
+
+def ramp_down():
+    for i in range(100,-1,-1):
+        D = i
+        d_1 = D
+        d_2 = D
+        activate_motors()
 
 def set_motordirection():
-    return
+    d_1 = cos(angle_err)
+    d_2 = sin(angle_err)
 
-def rotate(angle):
-    return
+def rotate():
+    angle_change = goal_angle - angle
+    if angle_change > 180:
+        angle_change -= 360
+        if angle_change > 0:
+            d_1 = -50
+            d_2 = 50
+        else:
+            d_1 = 50
+            d_2 = -50
+    activate_motors()
+    sleep_time = angle_change / 360 * ROTATION_PERIOD
+    time.sleep(sleep_time)
+    d_1 = 0
+    d_2 = 0
+    activate_motors()
+
+
+def activate_motors():
+    if d_1 > 0:
+        motor1_fwd_pwm.ChangeDutyCycle(d_1)
+        motor1_rev_pwm.ChangeDutyCycle(0)
+    elif d_1 < 0:
+        motor1_fwd_pwm.ChangeDutyCycle(0)
+        motor1_rev_pwm.ChangeDutyCycle(-1*d_1)
+
+    if d_2 > 0:
+        motor2_fwd_pwm.ChangeDutyCycle(d_2)
+        motor2_rev_pwm.ChangeDutyCycle(0)
+    elif d_2 < 0:
+        motor2_fwd_pwm.ChangeDutyCycle(0)
+        motor2_rev_pwm.ChangeDutyCycle(-1*d_2)
+    return    
 
 #------------------------------------------------
 
 
 #Main system loop:
     while running == True:
+        if bt_timeout >= 
+        try:
+            server_sock=socket.socket(socket.AF_BLUETOOTH, socket.SOCK_STREAM, socket.BTPROTO_RFCOMM)
+            server_sock.connect((port,10))
+        except:
+            print("Could not connect bluetooth")
         get_pos()
-        if get_distance >= 2.5:
-            pos_change = get_distance()
-            get_speed()
-            get_angle()
+        if calc_distance(pos_x_new,pos_y_new,pos_x_old,pos_y_old) >= 2.5:
+            pos_change = calc_distance(pos_x_new,pos_y_new,pos_x_old,pos_y_old)
+            calc_speed()
+            angle = calc_angle(pos_x_new,pos_y_new,pos_x_old,pos_y_old)
 #------------------------------------------------
