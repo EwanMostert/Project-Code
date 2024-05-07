118ef73fc15730630854f854aa3a0eb240861f65
EwanMostert <ewanmostert07@gmail.com>
2024-05-07 22:31:45 +0200

    yay server is now getting lots of images

--- Python/bt_image_theft_server.py
+++ Python/bt_image_theft_server.py
@@ -18,66 +18,34 @@ server_sock.listen(2)
 server_addr = server_sock.getsockname()[0]
 port = server_sock.getsockname()[1]
 logging.info(f"Listening on {server_addr}(port = {port})")
-used = 1
+connected = False
 
 while True:
-    client, addr = server_sock.accept()
-    client.setblocking(True)
-    logging.info(f"Got new connection from {addr}")
+    if connected == False:
+        client, addr = server_sock.accept()
+        client.setblocking(True)
+        logging.info(f"Got new connection from {addr}")
+        connected = True
     
     try:
         myfile = open(basename % imgcounter, 'w+b')
-        data = client.recv(8388608)
-        while data :
-            myfile.write(data)
-            data = client.recv(8388608)
-        myfile.close()
-        print("hello")
-    #     if used == 1:
-    #         # print(f"Buffer size is {buffer_size}")
-    #         data = client.recv(4096)
-    #         txt = str(data)
-    #         used = 0
-
-    #     if data:
-    #         if txt.__contains__('SIZE'):
-    #             # print(f"{txt}")
-    #             txt = txt.replace("'","")
-    #             # print(txt)
-    #             tmp = txt.split()
-    #             # print(tmp)
-    #             size = int(tmp[1])
-
-    #             # print("got size")
-    #             # print("size is %s" % size)
-
-    #             client.sendall("GOT SIZE".encode())
-    #             # Now set the buffer size for the image 
-    #             data = None
-
-    #         elif txt.__contains__('BYE'):
-    #             client.shutdown()
-
-    #     else:
-    #         print("hello")
-    #         myfile = open(basename % imgcounter, 'wb')
-    #         myfile.write(data)
-
-    #         data = client.recv(size)
-    #         if not data:
-    #             myfile.close()
-    #             break
-    #         myfile.write(data)
-    #         myfile.close()
-
-    #         client.sendall("GOT IMAGE".encode())
-    #         client.shutdown()    
+        imgcounter += 1
+        data = client.recv(1024)
+        txt = data.decode()
+        print(txt)
+        if txt.__contains__("sending data"):
+            message = (b'go ahead')
+            client.send(message)
+            data = client.recv(4096)
+            while data :
+                myfile.write(data)
+                data = client.recv(4096)
+            myfile.close()
+            print("File received")
+            client.detach()
+            connected = False
+  
     except: 
-        print("hello")
-        client.close()
-        break
-        continue
-
-    # imgcounter += 1
-
-server_sock.close()
+        print("Client error")
+        client.detach()
+        connected = False
