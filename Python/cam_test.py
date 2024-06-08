6615203aa615209cc8d3f26cdc925d3683637871
EwanMostert <ewanmostert07@gmail.com>
2024-06-08 14:53:49 +0200

    g

--- Python/cam_test.py
+++ Python/cam_test.py
@@ -1,10 +1,16 @@
 from picamera2 import *
 import time
 
+i = 0
+
 picam2 = Picamera2()
 camera_config = picam2.create_preview_configuration()
 picam2.configure(camera_config)
-picam2.start_preview(Preview.QTGL)
-picam2.start()
-time.sleep(2)
-picam2.capture_file("test.jpg")
+
+while True:
+
+	#picam2.start_preview(Preview.QTGL)
+	picam2.start()
+	time.sleep(10.0)
+	picam2.capture_file("image" + str(i) + ".jpg")
+	i += 1
